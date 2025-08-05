f742417396314737a179550e45664293fed5ea68
Asril Yusuf Harahap <asrilyusufhrp00@gmail.com>
2025-08-05 14:13:58 +0700

    Add files via upload
    tugas kuliah

---
+++ while.py
@@ -0,0 +1,12 @@
+# Nama : ASRIL YUSUF HARAHAP
+# NPM  : 2309030027
+# Prodi: Sains Data
+print('mulai')
+nomor_angkot = 1
+while nomor_angkot <= 10:
+  if nomor_angkot >= 1 and nomor_angkot <= 5:
+     print('angkot no.',nomor_angkot,'beroprasi dengan baik')
+  else:
+     print('angkot no.',nomor_angkot,'dalam keadaan rusak')
+  nomor_angkot += 1
+print('selesai')
